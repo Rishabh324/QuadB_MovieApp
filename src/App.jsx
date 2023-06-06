5eccded538ee21d84358243d838574948e4a4cd3
Rishabh Sinha <sinharishabh402@gmail.com>
2023-06-07 03:44:20 +0530

    Commit 3rd

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Single from './pages/Single';
 import Navbar from './components/Navbar';
-import Status from './components/Status';
 
 const App = () => {
   return (
@@ -15,7 +14,6 @@ const App = () => {
           <Route path='/' element={<Home />} />
           <Route path='/about' element={<About />} />
           <Route path='/singleshow/:id' element={<Single />} />
-          <Route path='/booking' element={<Status />} />
         </Routes>
       </div>
     </BrowserRouter>
